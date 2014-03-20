347b7da51732d58ed4d4623201dcc340ad6c1c40
TCuevas92 <TCuevas92@0abac4b6-36ec-cf2e-875f-aff034a38228>
2014-03-20 20:56:12 +0000

    Add "Game" menu to the game board screen with "Quit" menu item

--- trunk/src/main/java/edu/drexel/cs451_rbbtd/apologies/gui/Apologies.java
+++ trunk/src/main/java/edu/drexel/cs451_rbbtd/apologies/gui/Apologies.java
@@ -2,20 +2,24 @@ package edu.drexel.cs451_rbbtd.apologies.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 
 
 public class Apologies extends JFrame {
     public static ArrayList<JTextField> names = new ArrayList<JTextField>();
+    private JMenuBar menuBar;
+    private JMenu menu;
 
     public Apologies(ArrayList<PlayerColor> playerColors, PlayerColor first, ArrayList<JTextField> pNames) {
         names = pNames;
         // initialize art assets
         String boardIMG = getResourcePath("ApologiesBoard.png");
-        //String boardIMG = "resources/ApologiesBoard.png";
+//        String boardIMG = "resources/ApologiesBoard.png";
         Image board = new ImageIcon(boardIMG).getImage();
-
+        setupMenu();
         add(new Board(board, playerColors, first));
         setTitle("Apologies");
         setSize(600, 675);
@@ -37,5 +41,21 @@ public class Apologies extends JFrame {
         names.remove(nextName);
         names.add(nextName);
     }
+
+    private void setupMenu() {
+        menuBar = new JMenuBar();
+        menu = new JMenu("Game");
+        JMenuItem quit = new JMenuItem("Quit");
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        menu.add(quit);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+    }
+
 } // end class
 
